33f65f7d4e9252bb1a1c0eaed8f49bc0cba8f5f3
root <root@srv1013980.hstgr.cloud>
2026-04-06 20:41:39 +0000

    fix: node18, nginx proxy, socket url, wwebjs session

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,16 +1,12 @@
 function getConfig(name, defaultValue = null) {
-  // If inside a docker container, use window.ENV
   if (window.ENV !== undefined) {
     return window.ENV[name] || defaultValue;
   }
-
   return import.meta.env[name] || defaultValue;
 }
-
 export function getBackendUrl() {
-  return getConfig("VITE_BACKEND_URL");
+  return getConfig("REACT_APP_BACKEND_URL") || getConfig("VITE_BACKEND_URL");
 }
-
 export function getHoursCloseTicketsAuto() {
-  return getConfig("VITE_HOURS_CLOSE_TICKETS_AUTO");
+  return getConfig("VITE_HOURS_CLOSE_TICKETS_AUTO") || getConfig("REACT_APP_HOURS_CLOSE_TICKETS_AUTO");
 }
